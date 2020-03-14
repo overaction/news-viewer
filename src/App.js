64340a3b71a62e5d05b2fd797ef3b88cfde29cec
overaction <kmc04@naver.com>
2020-03-14 05:28:10 +0000

    axios.get

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [data,setData] = useState();
+    const onClick = () => {
+        axios.get('https://jsonplaceholder.typicode.com/todos/1').then(respon => {
+            setData(respon.data);
+        });
+    }
+    return (
+        <div>
+            <div>
+                <button onClick={onClick}>불러오기</button>
+            </div>
+            {data && <textarea rows={7} value={JSON.stringify(data,null,1)} readOnly={true} />}
+        </div>
+    )
 }
 
 export default App;
